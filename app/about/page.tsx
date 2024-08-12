55b9b9449738fb610a6b1f5b95ab8ac1789ca4de
chiqueto <lfchiqueto@gmail.com>
2024-08-12 15:55:04 -0300

    chore: add shadcn setup

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "../_components/ui/button";
+
 export default function About() {
     return (
-      <h1>about page</h1>
+      
+      <><h1>about page</h1>
+      aaa<Button>test</Button></>
     );
   }
   
