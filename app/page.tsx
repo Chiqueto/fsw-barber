90b351f365afac082743d6b4efc95825b5fbd19b
chiqueto <lfchiqueto@gmail.com>
2024-08-26 09:25:32 -0300

    feat: add Booking-item components

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
-import { Badge } from "./_components/ui/badge"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/BarberhopItem"
 import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   // Chamar meu banco de dados
@@ -25,7 +24,7 @@ export default async function Home() {
       {/* Header */}
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Felipe</h2>
+        <h2 className="text-xl font-bold">Olá, Luís Felipe</h2>
         <p>Segunda-feira 05 de agosto.</p>
 
         {/* BUSCA */}
@@ -61,32 +60,8 @@ export default async function Home() {
         </div>
 
         {/* AGENDAMENTO */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
 
-        <Card className="">
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/* DIREITA */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
