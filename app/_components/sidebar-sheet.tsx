bc1cf83121f153fc0c552c221ae5fc8b91f9e344
chiqueto <lfchiqueto@gmail.com>
2024-09-16 08:58:51 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,4 +1,4 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import Image from "next/image"
 import {
   SheetClose,
@@ -8,9 +8,16 @@ import {
   SheetTitle,
 } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -18,15 +25,41 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
         <SheetDescription>
-          <div className="flex items-center gap-3 border-b border-solid py-5 text-left">
-            <Avatar>
+          <div className="flex items-center justify-between gap-3 border-b border-solid py-5 text-left">
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size={"icon"}>
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <DialogHeader>
+                  <DialogTitle>Faça seu login na plataforma</DialogTitle>
+                  <DialogDescription>
+                    Conecte-se usando sua conta do Google
+                  </DialogDescription>
+                </DialogHeader>
+                <Button variant={"outline"} className="gap-1 font-bold">
+                  <Image
+                    src={"google.svg"}
+                    width={18}
+                    height={18}
+                    alt="fazer login com o Google"
+                  ></Image>{" "}
+                  Google
+                </Button>
+              </DialogContent>
+            </Dialog>
+            {/* <Avatar>
               <AvatarImage src="https://media.licdn.com/dms/image/v2/D5603AQEhKwLb1ZUqjA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1671235367473?e=1730937600&v=beta&t=8K0ueM1lE6eJgiNPHG_4Zp0Qm8vhZAH5yjAFznGh3F0" />
             </Avatar>
 
             <div>
               <p className="font-bold">Luís Felipe</p>
               <p className="text-xs">lfchiqueto@gmail.com</p>
-            </div>
+            </div> */}
           </div>
 
           <div className="flex flex-col gap-2 border-b border-solid py-5">
