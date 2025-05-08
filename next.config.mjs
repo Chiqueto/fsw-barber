4b496ae83ded3e6d4aa46376ac840207a5831468
Luis Felipe <83621659+Chiqueto@users.noreply.github.com>
2025-05-08 18:48:06 +0000

    chore: fix cors for codespace origin

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {//configurando para que o next possa exibir as imagens que vem do meu servidor
-        remotePatterns: [
-            {
-                hostname: "utfs.io",
-            },
-        ],
+  experimental: {
+    //configurando origens
+    serverActions: {
+      allowedOrigins: [
+        "https://didactic-couscous-rqrr65vqgp2pv44.github.dev/",
+        "https://didactic-couscous-rqrr65vqgp2pv44-3000.app.github.dev/",
+        "localhost:3000",
+      ],
     },
+  },
+  images: {
+    //configurando para que o next possa exibir as imagens que vem do meu servidor
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
+  },
+}
 
-};
-
-
-
-export default nextConfig;
+export default nextConfig
